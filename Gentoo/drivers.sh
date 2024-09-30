fbef2d5177eeedd44cabe5651ed1aec2e87ac645
StealYourCode <90696735+StealYourCode@users.noreply.github.com>
2024-09-30 17:24:22 +0200

    Add files via upload

---
+++ Gentoo/drivers.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e  
+
+
+# Switch to chroot environment
+chroot /mnt/gentoo /bin/bash 
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+
+# Sync the latest Gentoo repository
+echo "Syncing Gentoo repository..."
+emerge-webrsync
+
+# TIME ZONE SETUP
+echo "Setting timezone to ${TIMEZONE}..."
+ln -sf "/usr/share/zoneinfo/Europe/Brussels" "/etc/localtime"
+
+# KEYBOARD SETUP
+echo "Configuring locale and keyboard..."
+# Generate locales
+sed -i '/^#en_US.UTF-8/s/^#//' /etc/locale.gen   # Uncomment 'en_US.UTF-8'
+echo "fr_BE.UTF-8 UTF-8" >> /etc/locale.gen
+
+# Update environments
+echo "Updating environment..."
+env-update && source /etc/profile && export PS1="(chroot) ${PS1}"
+
+# INSTALL REQUIRED PACKAGES
+echo "Installing necessary system utilities..."
+emerge --ask sys-apps/pciutils sys-apps/usbutils  # Install PCI and USB utilities
+
+# INSTALL KERNEL MODULES
+echo "Installing Gentoo kernel sources..."
+emerge --ask sys-kernel/gentoo-sources 
+
+# CONFIGURE AND COMPILE KERNEL
+echo "Configuring and compiling the kernel..."
+### NEED TO GO TO DIFFERENT LINUX
+cd /usr/src/linux*
+make menuconfig  # Opens menu for manual kernel configuration
+### CURL THE CONFIG FILE FROM GITHUB ?
+
+
+# Compile kernel and install modules
+echo "Starting kernel compilation..."
+date
+make -j2 && make modules_install
+date
+echo "Kernel compilation and module installation completed."
+
+
+echo "Exiting chroot environment."
+
